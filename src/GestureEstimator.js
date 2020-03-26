ccd1f7e3573dd80c36fd8849a4afbb0eda7f849b
andypotato <8491849+andypotato@users.noreply.github.com>
2020-03-26 08:12:14 +0800

    Merge finger pose estimation and gesture estimation

--- src/GestureEstimator.js
+++ src/GestureEstimator.js
@@ -1,18 +1,26 @@
+import FingerPoseEstimator from './FingerPoseEstimator';
+
 export default class GestureEstimator {
 
-  constructor(knownGestures) {
+  constructor(knownGestures, estimatorOptions = {}) {
+
+    this.estimator = new FingerPoseEstimator(estimatorOptions);
 
     // list of predefined gestures
     this.gestures = knownGestures;
   }
 
-  estimate(detectedCurls, detectedDirections, minConfidence) {
+  estimate(landmarks, minConfidence) {
 
     let gesturesFound = [];
 
+    // step 1: get estimations of curl / direction for each finger
+    const est = this.estimator.estimate(landmarks);
+
+    // step 2: compare gesture description to each known gesture
     for(let gesture of this.gestures) {
 
-      let confidence = gesture.matchAgainst(detectedCurls, detectedDirections);
+      let confidence = gesture.matchAgainst(est.curls, est.directions);
       if(confidence >= minConfidence) {
         gesturesFound.push({
           name: gesture.name,
