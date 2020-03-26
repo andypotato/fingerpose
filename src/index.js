d8faf418cee4dde1b5cd2365388ed7fed95c6fe5
andypotato <8491849+andypotato@users.noreply.github.com>
2020-03-26 08:12:31 +0800

    Update webpack config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import FingerPoseEstimator from './FingerPoseEstimator';
 import GestureEstimator from './GestureEstimator';
 
-export default {
-  FingerPoseEstimator, GestureEstimator
-}
+export default GestureEstimator;
