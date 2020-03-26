24e0e34395c500a290b5369e91375d3ff14b2e78
andypotato <8491849+andypotato@users.noreply.github.com>
2020-03-26 14:26:53 +0800

    Added debug print option

--- src/GestureEstimator.js
+++ src/GestureEstimator.js
@@ -1,4 +1,5 @@
 import FingerPoseEstimator from './FingerPoseEstimator';
+import { Finger, FingerCurl, FingerDirection } from './FingerDescription';
 
 export default class GestureEstimator {
 
@@ -10,16 +11,25 @@ export default class GestureEstimator {
     this.gestures = knownGestures;
   }
 
-  estimate(landmarks, minConfidence) {
+  estimate(landmarks, minConfidence, printDebugInfo=false) {
 
     let gesturesFound = [];
 
     // step 1: get estimations of curl / direction for each finger
     const est = this.estimator.estimate(landmarks);
 
+    if(printDebugInfo) {
+      for(let fingerIdx of Finger.all) {
+        console.log(
+          Finger.getName(fingerIdx),
+          FingerCurl.getName(est.curls[fingerIdx]),
+          FingerDirection.getName(est.directions[fingerIdx])
+        );
+      }
+    }
+
     // step 2: compare gesture description to each known gesture
     for(let gesture of this.gestures) {
-
       let confidence = gesture.matchAgainst(est.curls, est.directions);
       if(confidence >= minConfidence) {
         gesturesFound.push({
