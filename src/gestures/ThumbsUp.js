e6dbfc2ecd5c1890e00acb52ea9bb2a40707443c
andypotato <8491849+andypotato@users.noreply.github.com>
2021-10-29 19:18:48 +0200

    Improved sample gestures

--- src/gestures/ThumbsUp.js
+++ src/gestures/ThumbsUp.js
@@ -6,20 +6,28 @@ import GestureDescription from '../GestureDescription';
 const thumbsUpDescription = new GestureDescription('thumbs_up');
 
 // thumb:
-// - not curled
-// - vertical up (best) or diagonal up left / right
+// - curl: none (must)
+// - direction vertical up (best)
+// - direction diagonal up left / right (acceptable)
 thumbsUpDescription.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
 thumbsUpDescription.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
-thumbsUpDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 0.25);
-thumbsUpDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 0.25);
+thumbsUpDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 0.9);
+thumbsUpDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 0.9);
 
 // all other fingers:
-// - curled
-// - horizontal left or right
+// - curled (best)
+// - half curled (acceptable)
+// - pointing down is NOT acceptable
 for(let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
   thumbsUpDescription.addCurl(finger, FingerCurl.FullCurl, 1.0);
-  thumbsUpDescription.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
-  thumbsUpDescription.addDirection(finger, FingerDirection.HorizontalRight, 1.0);
+  thumbsUpDescription.addCurl(finger, FingerCurl.HalfCurl, 0.9);
 }
+
+// require the index finger to be somewhat left or right pointing
+// but NOT down and NOT fully up
+thumbsUpDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 1.0);
+thumbsUpDescription.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0);
+thumbsUpDescription.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0);
+thumbsUpDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 1.0);
 
 export default thumbsUpDescription;
--- src/gestures/Victory.js
+++ src/gestures/Victory.js
@@ -7,35 +7,32 @@ const victoryDescription = new GestureDescription('victory');
 
 
 // thumb:
-victoryDescription.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 0.5);
-victoryDescription.addCurl(Finger.Thumb, FingerCurl.NoCurl, 0.5);
 victoryDescription.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
 victoryDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 1.0);
+victoryDescription.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight, 1.0);
 
 // index:
 victoryDescription.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
-victoryDescription.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.75);
+victoryDescription.addDirection(Finger.Index, FingerDirection.VerticalUp, 1.0);
 victoryDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 1.0);
+victoryDescription.addDirection(Finger.Index, FingerDirection.DiagonalUpRight, 1.0);
+victoryDescription.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 1.0);
+victoryDescription.addDirection(Finger.Index, FingerDirection.HorizontalRight, 1.0);
 
 // middle:
 victoryDescription.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0);
 victoryDescription.addDirection(Finger.Middle, FingerDirection.VerticalUp, 1.0);
-victoryDescription.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 0.75);
+victoryDescription.addDirection(Finger.Middle, FingerDirection.DiagonalUpLeft, 1.0);
+victoryDescription.addDirection(Finger.Middle, FingerDirection.DiagonalUpRight, 1.0);
+victoryDescription.addDirection(Finger.Middle, FingerDirection.HorizontalLeft, 1.0);
+victoryDescription.addDirection(Finger.Middle, FingerDirection.HorizontalRight, 1.0);
 
 // ring:
 victoryDescription.addCurl(Finger.Ring, FingerCurl.FullCurl, 1.0);
-victoryDescription.addDirection(Finger.Ring, FingerDirection.VerticalUp, 0.2);
-victoryDescription.addDirection(Finger.Ring, FingerDirection.DiagonalUpLeft, 1.0);
-victoryDescription.addDirection(Finger.Ring, FingerDirection.HorizontalLeft, 0.2);
+victoryDescription.addCurl(Finger.Ring, FingerCurl.HalfCurl, 0.9);
 
 // pinky:
 victoryDescription.addCurl(Finger.Pinky, FingerCurl.FullCurl, 1.0);
-victoryDescription.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 0.2);
-victoryDescription.addDirection(Finger.Pinky, FingerDirection.DiagonalUpLeft, 1.0);
-victoryDescription.addDirection(Finger.Pinky, FingerDirection.HorizontalLeft, 0.2);
-
-// give additional weight to index and ring fingers
-victoryDescription.setWeight(Finger.Index, 2);
-victoryDescription.setWeight(Finger.Middle, 2);
+victoryDescription.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 0.9);
 
 export default victoryDescription;
